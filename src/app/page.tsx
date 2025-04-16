ed1f5fe1ab30df62d63400a46ef79c1f1a825728
Matheus Felipe Zanin <matt_z6@hotmail.com>
2025-04-16 17:51:54 -0300

    chore: home page details (#2)
    * chore: remove unused images
    * chore: add band favicon
    * chore: add band logo
    * chore: update meta title and description
    * chore: improve home page styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
+import Image from 'next/image'
+
 import { YoutubeLogo } from '@phosphor-icons/react/dist/ssr/YoutubeLogo'
 import { TiktokLogo } from '@phosphor-icons/react/dist/ssr/TiktokLogo'
 import { InstagramLogo } from '@phosphor-icons/react/dist/ssr/InstagramLogo'
 import { SpotifyLogo } from '@phosphor-icons/react/dist/ssr/SpotifyLogo'
 import { MusicNote } from '@phosphor-icons/react/dist/ssr/MusicNote'
 
+import bandLogo from '../../public/logo.webp'
+
 export default function HomePage() {
   return (
-    <main className="flex-1 flex flex-col gap-6 min-h-full max-w-3xl mx-auto py-20">
+    <main className="flex-1 flex flex-col gap-6 min-h-full max-w-3xl mx-auto py-20 px-4">
+        <Image src={bandLogo} alt="" className="size-28 md:size-40 rounded-xl self-center shadow-md" />
+
       <div className="flex flex-col items-center gap-2">
         <h2 className="text-center text-2xl text-foreground font-mono font-medium">Like a Tear</h2>
         <p className="text-center text-base text-muted-foreground font-mono">Hardcore melódico de Guarapuava, PR.</p>
@@ -29,19 +35,19 @@ export default function HomePage() {
         </a>
       </div>
 
-      <a href="https://open.spotify.com/intl-pt/track/43WN6G6fDiat8RPDyTFOT7" target="_blank" className="flex items-center justify-center self-center gap-2 min-h-8 rounded-full border border-border w-fit px-3">
-        <MusicNote className="size-4" />
+      <a href="https://open.spotify.com/intl-pt/track/43WN6G6fDiat8RPDyTFOT7" target="_blank" className="flex items-center justify-center self-center gap-2 min-h-8 rounded-full border border-border w-fit px-3 py-1 shadow-sm">
+        <MusicNote className="size-4 hidden md:block" />
         <span className="text-center text-base">Ouça nosso novo single, {'"'}Melhor do Que Antes{'"'}</span>
       </a>
 
       <nav className="flex flex-col gap-2 w-full max-w-xl mx-auto">
-        <a href="https://www.youtube.com/watch?v=f7t4GUuzAHQ" className="flex items-center justify-center rounded-md min-h-12 text-center hover:bg-gray-100">
+        <a href="https://www.youtube.com/watch?v=f7t4GUuzAHQ" className="flex items-center justify-center rounded-md min-h-12 py-2 text-center hover:bg-gray-100 border border-border md:border-0">
           Veja o lyric vídeo de {'"'}Melhor do Que Antes{'"'} no YouTube
         </a>
-        <a href="https://instagram.com/likeatear_" target="_blank" className="flex items-center justify-center rounded-md min-h-12 text-center hover:bg-gray-100">
+        <a href="https://instagram.com/likeatear_" target="_blank" className="flex items-center justify-center rounded-md min-h-12 py-2 text-center hover:bg-gray-100 border border-border md:border-0">
           Siga gente no Insta
         </a>
-        <a href="https://likeatear.com" target="_blank" className="flex items-center justify-center rounded-md min-h-12 text-center hover:bg-gray-100">
+        <a href="https://likeatear.com" target="_blank" className="flex items-center justify-center rounded-md min-h-12 py-2 text-center hover:bg-gray-100 border border-border md:border-0">
           Conheça nosso site
         </a>
       </nav>
