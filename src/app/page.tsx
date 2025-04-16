699c5edf34c1031b57cee4322c4813423c7ffa38
Matheus Felipe Zanin <matt_z6@hotmail.com>
2025-04-16 17:14:22 -0300

    chore: project startup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,50 @@
-import Image from "next/image";
+import { YoutubeLogo } from '@phosphor-icons/react/dist/ssr/YoutubeLogo'
+import { TiktokLogo } from '@phosphor-icons/react/dist/ssr/TiktokLogo'
+import { InstagramLogo } from '@phosphor-icons/react/dist/ssr/InstagramLogo'
+import { SpotifyLogo } from '@phosphor-icons/react/dist/ssr/SpotifyLogo'
+import { MusicNote } from '@phosphor-icons/react/dist/ssr/MusicNote'
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main className="flex-1 flex flex-col gap-6 min-h-full max-w-3xl mx-auto py-20">
+      <div className="flex flex-col items-center gap-2">
+        <h2 className="text-center text-2xl text-foreground font-mono font-medium">Like a Tear</h2>
+        <p className="text-center text-base text-muted-foreground font-mono">Hardcore melódico de Guarapuava, PR.</p>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+      <hr className="self-center w-28 h-[1px] border-0 bg-border" />
+
+      <div className="flex gap-2 items-center justify-center">
+        <a href="https://open.spotify.com/intl-pt/artist/6UT0bycABPfvalMWR3TWVG" target="_blank" className="flex items-center justify-center size-10 rounded-md hover:bg-muted text-foreground">
+          <SpotifyLogo weight="light" className="size-8" />
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
+        <a href="https://instagram.com/likeatear_" target="_blank" className="flex items-center justify-center size-10 rounded-md hover:bg-muted text-foreground">
+          <InstagramLogo weight="light" className="size-8" />
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+        <a href="https://www.youtube.com/@LikeaTear" target="_blank" className="flex items-center justify-center size-10 rounded-md hover:bg-muted text-foreground">
+          <YoutubeLogo weight="light" className="size-8" />
         </a>
-      </footer>
-    </div>
-  );
+        <a href="https://tiktok.com/@likeatear" target="_blank" className="flex items-center justify-center size-10 rounded-md hover:bg-muted text-foreground">
+          <TiktokLogo weight="light" className="size-8" />
+        </a>
+      </div>
+
+      <a href="https://open.spotify.com/intl-pt/track/43WN6G6fDiat8RPDyTFOT7" target="_blank" className="flex items-center justify-center self-center gap-2 min-h-8 rounded-full border border-border w-fit px-3">
+        <MusicNote className="size-4" />
+        <span className="text-center text-base">Ouça nosso novo single, {'"'}Melhor do Que Antes{'"'}</span>
+      </a>
+
+      <nav className="flex flex-col gap-2 w-full max-w-xl mx-auto">
+        <a href="https://www.youtube.com/watch?v=f7t4GUuzAHQ" className="flex items-center justify-center rounded-md min-h-12 text-center hover:bg-gray-100">
+          Veja o lyric vídeo de {'"'}Melhor do Que Antes{'"'} no YouTube
+        </a>
+        <a href="https://instagram.com/likeatear_" target="_blank" className="flex items-center justify-center rounded-md min-h-12 text-center hover:bg-gray-100">
+          Siga gente no Insta
+        </a>
+        <a href="https://likeatear.com" target="_blank" className="flex items-center justify-center rounded-md min-h-12 text-center hover:bg-gray-100">
+          Conheça nosso site
+        </a>
+      </nav>
+    </main>
+  )
 }
